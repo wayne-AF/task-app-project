3bc739c278fbb53db1ab2b7bacd048f079f6886b
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-12-30 18:19:10 +0000

    add pages

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidebar initialisation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
